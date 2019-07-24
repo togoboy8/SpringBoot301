fb7284406bf68596d9e3ae14248c95e81b5f1c6e
Cedric <cedrichillah@gmail.com>
2019-07-24 19:37:01 -0400

    Fist commit for Dog Database

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Dog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+
+    @NotNull
+    @Min(1)
+    private long id;
+
+    @NotNull
+    @Size(min = 3, max = 25)
+    private String name;
+
+    @Size(min = 3, max = 25)
+    private String type;
+
+    @NotNull
+    @Min(1)
+    private int age;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
